e418df290f6c72cbab82920e6780e3c3a1f0932e
Daniel Mar <daniel.k.mar@outlook.com>
2020-12-24 15:07:08 -0800

    completed day 5 part 2

--- src/day05.zig
+++ src/day05.zig
@@ -7,12 +7,36 @@ const math = std.math;
 const input = @embedFile("../in/day05.txt");
 
 pub fn main() !void {
+    const max_seat_id = math.maxInt(u10);
+    var seat_taken = [_]bool{false} ** max_seat_id;
+    var min: u10 = max_seat_id;
     var max: u10 = 0;
+
     var it = mem.split(input, "\n");
-    while (it.next()) |pass| max = math.max(max, seatID(pass));
-    print("part1: {}", .{max});
+    while (it.next()) |pass| {
+        const seat_id = seatID(pass);
+        min = math.min(min, seat_id);
+        max = math.max(max, seat_id);
+        seat_taken[seat_id] = true;
+    }
+    print("part1: {}\n", .{max});
+
+    const seat = blk: {
+        for (seat_taken[min .. max]) |taken, i| {
+            if (!taken) break :blk (min + @intCast(u10, i));
+        }
+        unreachable;
+    };
+    print("part2: {}\n", .{seat});
 }
 
+// 127 = 0b1111111
+//         BBBBBBB
+//   0 = 0b0000000
+//         FFFFFFF
+// So if a given char of the code is 'B', then the corresponding bit is 1.
+// And if a given char of the code is 'F', then the corresponding bit is 0.
+// Same concept applies to 'R' and 'L' for the remaining three bits.
 inline fn seatID(boarding_pass: []const u8) u10 {
     var id: u10 = 0;
     for (boarding_pass) |char| {
